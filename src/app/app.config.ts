36db972eea5c5052967bf62f87d567623fbda45b
Felix Jordan <felixjordan.dev@gmail.com>
2025-10-18 22:10:36 +0000

    feat: Set up shared Angular Material module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideAnimations()
+  ],
+  imports: [
+    SharedModule
   ]
 };
