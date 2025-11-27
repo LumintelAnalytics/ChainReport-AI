d2904afba42815fede5129f396652b5c08039fbe
Kevin Ackerman <ackermankevin176@gmail.com>
2025-11-27 03:44:09 +0000

    feat: Add code review section component

---
+++ src/app/features/report-viewer/code-review-section/code-review-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-review-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './code-review-section.component.html',
+  styleUrl: './code-review-section.component.scss'
+})
+export class CodeReviewSectionComponent {
+
+}
